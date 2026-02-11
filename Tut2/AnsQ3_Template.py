3fa5b8180f3a9c4d197a9c34bf09b22328939c9c
liang799 <71926348+liang799@users.noreply.github.com>
2026-02-11 21:38:14 +0800

    feat: completed reverse stack question

---
+++ Tut2/AnsQ3_Template.py
@@ -0,0 +1,103 @@
+class ListNode:
+    def __init__(self, item=None):
+        self.item = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.size = 0
+        self.head = None
+
+    def print_list(self):
+        cur = self.head
+        if cur is None:
+            print("Empty")
+            return
+        while cur is not None:
+            print(cur.item, end=" ")
+            cur = cur.next
+        print()
+
+    def find_node(self, index):
+        if index < 0 or index >= self.size:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def insert_node(self, index, value):
+        if index < 0 or index > self.size:
+            return -1
+        new_node = ListNode(value)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            self.size += 1
+            return 0
+        prev_node = self.find_node(index - 1)
+        if prev_node is None:
+            return -1
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+        self.size += 1
+        return 0
+
+    def remove_node(self, index):
+        if index < 0 or index >= self.size:
+            return -1
+        if index == 0:
+            self.head = self.head.next
+            self.size -= 1
+            return 0
+        prev_node = self.find_node(index - 1)
+        if prev_node is None or prev_node.next is None:
+            return -1
+        prev_node.next = prev_node.next.next
+        self.size -= 1
+        return 0
+
+    def remove_all_items(self):
+        self.head = None
+        self.size = 0
+
+class Stack:
+    def __init__(self):
+        self.ll = LinkedList()
+
+    def push(self, item):
+        self.ll.insert_node(0, item)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        item = self.ll.head.item
+        self.ll.remove_node(0)
+        return item
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.ll.head.item
+
+    def is_empty(self):
+        return self.ll.size == 0
+
+def sort_stack(stack):
+    tmp = Stack()
+    while not stack.is_empty():
+        tmp.push(stack.pop())
+
+    stack.ll = tmp.ll
+
+def main():
+    stack = Stack()
+    for i in range(1,6):
+        stack.push(i)
+    stack.ll.print_list()
+
+    sort_stack(stack)
+    stack.ll.print_list()
+
+if __name__ == "__main__":
+    main()
